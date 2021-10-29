b62801e719ebcbc681158713f9502b396293be97
Joy10311 <joy10311@gmail.com>
2021-10-29 10:06:06 +0600

    first commit

---
+++ src/Pages/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoEbPhix4PdjeM1vs0AbjO4kWYN0d-CmI",
+    authDomain: "g-tour-1abb5.firebaseapp.com",
+    projectId: "g-tour-1abb5",
+    storageBucket: "g-tour-1abb5.appspot.com",
+    messagingSenderId: "477260168429",
+    appId: "1:477260168429:web:e66a8eba88864055f860e4"
+};
+
+
+export default firebaseConfig;
---
+++ src/Pages/Home/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo@2x_white.png'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="bg-green-600 text-white  text-2xl font-medium ">
+                <Link to="/home"><img className="pt-5" src={logo} alt="" /></Link>
+                <div className="mb-10  text-center pb-7">
+                    <Link className="mx-10 lg:inline block" to="/home">Home</Link>
+                    <Link className="mx-10 lg:inline block" to="/tours">Tours</Link>
+                    <Link className="mx-10 lg:inline block" to="/blog">Blog</Link>
+                    <Link className="mx-10 lg:inline block" to="/about">About Us</Link>
+                    <Link className="mx-10 lg:inline block" to="/contact">Contact Us</Link>
+                    {/* {user?.email ?
+                        <button onClick={logOut} className="bg-blue-300 text-base rounded px-1 py-1 mr-2">Log Out</button> :
+                        <Link className="mx-10 lg:inline block" to="/login">Log In</Link>}
+                    <Link to="" className="text-red-700"> {user?.displayName}</Link> */}
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
